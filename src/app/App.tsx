0acd69c9b8dd9cb7833f4871874f9b1ac0cd1dd3
Walied Rashied <w.rashied@icloud.com>
2021-11-03 16:07:13 +0100

    Title component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,15 +5,12 @@ import Title from './components/Title/Title';
 function App(): JSX.Element {
   return (
     <div>
-      <Title>
-        Hier steht H1 Text
-      </Title>
-    <Button>
-      <div>Hello</div>
+      <Title>Hier steht H1 Text</Title>
+      <Button>
+        <div>Hello</div>
       </Button>
     </div>
   );
 }
-
 
 export default App;
